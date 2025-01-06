2fbc738ce6473c7ec933915ea0f112f8d7b10b1e
PabloCavillon <pabloa.cavillon@gmail.com>
2025-01-06 00:21:33 -0300

    prueba online

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,8 @@
-export * from './input-editable/InputEditable'
+export * from './input-editable/InputEditable';
 
-export * from './not-found/NotFound'
+export * from './not-found/NotFound';
 
-export * from './top-menu/TopMenu'
+export * from './top-menu/TopMenu';
 
 export * from './product-form/ProductForm';
 
